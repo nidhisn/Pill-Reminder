5be763c63193d55cef696cc9b3c3dcaeb85f1323
nidhisn <nidhi24sn@gmail.com>
2024-07-22 15:17:30 +0530

    first commit

---
+++ app/src/main/java/com/example/pillreminder/AddPillReminder.kt
@@ -0,0 +1,76 @@
+package com.example.pillreminder
+
+import android.hardware.biometrics.BiometricManager
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.pillreminder.databinding.ActivityAddPillReminderBinding
+import com.example.pillreminder.databinding.ActivityMainBinding
+
+class AddPillReminder : AppCompatActivity() {
+
+    private lateinit var binding:ActivityAddPillReminderBinding
+
+    private val pickerVals = arrayOf("AM", "PM")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_add_pill_reminder)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding= ActivityAddPillReminderBinding.inflate(layoutInflater)
+        val view=binding.root
+        setContentView(view)
+
+
+
+        binding.numPickHour.maxValue=12
+        binding.numPickHour.minValue=0
+
+        binding.numPickHour.maxValue=60
+        binding.numPickHour.maxValue=0
+
+        // Set the AM/PM values for the third NumberPicker
+        binding.numPickAm.minValue = 0
+        binding.numPickAm.maxValue = pickerVals.size - 1
+        binding.numPickAm.displayedValues=pickerVals
+
+        // Retrieve the string array from resources
+        val reminderTypes = resources.getStringArray(R.array.reminder_types)
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        val arrayAdapter= ArrayAdapter(this, android.R.layout.simple_spinner_item,reminderTypes)
+        // Specify the layout to use when the list of choices appears
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        // Apply the adapter to the spinner
+        binding.spinnerType.adapter=arrayAdapter
+
+        binding.spinnerType.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val selectedItem=parent?.getItemAtPosition(position).toString()
+                binding.tvTypeTitle.setText(selectedItem+" Pill")
+
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/pillreminder/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.pillreminder
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.pillreminder.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+    binding=ActivityMainBinding.inflate(layoutInflater)
+        val view=binding.root
+        setContentView(view)
+
+        binding.btnAddReminder.setOnClickListener {
+            val intent= Intent(this@MainActivity,AddPillReminder::class.java)
+            startActivity(intent)
+        }
+    }
+}
